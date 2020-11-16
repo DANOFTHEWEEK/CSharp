bf8075440c02a0798678362fdd0982e3d768638b
PyxlsDev <ytxlezrymid@gmail.com>
2020-11-15 21:09:57 -0500

    csharp

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ReadLine();
+
+           Console.WriteLine("hello");
+
+           Console.ReadLine();
+
+           Console.WriteLine("yea we know that already dip shit.");
+
+           Console.ReadLine();
+
+           Console.WriteLine("You really thought you were smart? loser.");
+              
+            // Wait before closing
+            Console.ReadKey();
+        }
+    }
+}
